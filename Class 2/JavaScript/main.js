19a824d4f30692a2fe07008aa114093d69733c50
LopezWellsSebastian <sebastianwells33@hotmail.com>
2022-07-27 17:28:29 -0700

    Class 2 Assignment
    - Have a capture form that will be user to create new Tasks or show info of existing ones.
    - Have an icon that changes the state to indicate Important and non-important tasks.
    - Have button to show and hide the capture form

---
+++ Class 2/JavaScript/main.js
@@ -0,0 +1,83 @@
+var regularIcon = "fa-regular fa-star";
+var solidIcon = "fa-solid fa-star";
+var isImportantIcon = false;
+var hiddenForm = false;
+function toggleImportant() {
+  if (!isImportantIcon) {
+    $("#Important").removeClass(regularIcon);
+    $("#Important").addClass(solidIcon);
+    isImportantIcon = true;
+  } else {
+    $("#Important").removeClass(solidIcon);
+    $("#Important").addClass(regularIcon);
+    isImportantIcon = false;
+  }
+}
+function hideForm() {
+  console.log("Hide Form");
+
+  if (!hiddenForm) {
+    hiddenForm = true;
+    console.log(hiddenForm);
+    $(".form-container").hide();
+  } else {
+    hiddenForm = false;
+    console.log(hiddenForm);
+    $(".form-container").show();
+  }
+}
+function saveInfo() {
+  let title = $("#txtTitle").val();
+  let description = $("#txtDescription").val();
+  let dueDate = $("#txtDueDate").val();
+  let color = $("#txtColor").val();
+  let emoji = $("#txtEmoji").val();
+  let location = $("#txtLocation").val();
+  let status = $("#txtStatus").val();
+  let notification = $("#notification").val();
+
+  let task = new Task(
+    isImportantIcon,
+    title,
+    description,
+    dueDate,
+    color,
+    emoji,
+    location,
+    status,
+    notification
+  );
+  console.log(task);
+  displayTask(task);
+}
+function displayTask(task) {
+  let card = `
+  <div class="card">
+    <div>
+      <h3>Title: ${task.title}</h3>
+      <p>Description: ${task.description}</p>
+    </div>
+    <div>
+      <p>Due Date: ${task.dueDate}</p>
+      <p>color: ${task.color}</p>
+      <p>Emoji: ${task.emoji}</p> 
+    </div>
+    <div>
+    <p>Location: ${task.location}</p>
+    <p>Status: ${task.status}</p>
+    <p>Notification: ${task.notification.toUpperCase()}</p>
+    </div>
+  </div >
+  `;
+  $("#Cards").append(card);
+}
+function init() {
+  console.log("Task Mangeer Page");
+
+  $("#Important").click(toggleImportant);
+
+  $("#btnSave").click(saveInfo);
+
+  $("#hideBTN").click(hideForm);
+}
+window.onload = init;
---
+++ Class 2/JavaScript/task.js
@@ -0,0 +1,25 @@
+class Task {
+  constructor(
+    important,
+    title,
+    description,
+    dueDate,
+    color,
+    emoji,
+    location,
+    status,
+    notification
+  ) {
+    this.important = important;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.color = color;
+    this.emoji = emoji;
+    this.location = location;
+    this.status = status;
+    this.notification = notification;
+
+    this.name = "sergio";
+  }
+}
